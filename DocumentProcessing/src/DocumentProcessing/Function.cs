6fb973f498d1e1896b27ba298a4563e72ae5db07
Workshop Participant <participant@example.com>
2025-12-03 07:26:14 +0000

    Implement function

--- DocumentProcessing/src/DocumentProcessing/Function.cs
+++ DocumentProcessing/src/DocumentProcessing/Function.cs
@@ -16,7 +16,37 @@ namespace DocumentProcessing
         {
             try
             {
-                
+                context.Logger.LogInformation($"Lambda received event ApplicationId: {applicationEvent.Detail.ApplicationId}, DocType: {applicationEvent.Detail.DocType}, Path: {applicationEvent.Detail.Path}");
+
+                var s3BucketName = Environment.GetEnvironmentVariable("S3BucketName");
+                var minIncomeStr = Environment.GetEnvironmentVariable("MinIncome");
+                var minIncome = decimal.Parse(minIncomeStr);
+
+                string query;
+                if (applicationEvent.Detail.DocType == DocumentType.INCOMESTATEMENT)
+                {
+                    query = Environment.GetEnvironmentVariable("IncomeQuery");
+                }
+                else if (applicationEvent.Detail.DocType == DocumentType.IDENTITYDOCUMENT)
+                {
+                    query = Environment.GetEnvironmentVariable("IdDocQuery");
+                }
+                else
+                {
+                    context.Logger.LogWarning($"Unknown DocType: {applicationEvent.Detail.DocType}");
+                    return;
+                }
+
+                using var processor = new DocumentProcessor(context.Logger);
+                var _result = processor.ProcessDocument(
+                    applicationEvent.Detail.ApplicationId,
+                    applicationEvent.Detail.DocType,
+                    s3BucketName,
+                    applicationEvent.Detail.Path,
+                    query,
+                    minIncome);
+
+                processor.PublishEvent(_result);
             }
             catch (Exception ex)
             {
